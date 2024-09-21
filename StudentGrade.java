61fa46508ba450e8b2a75ef796c9e905821dcfb4
Vaasanthi21 <167667051+Vaasanthi21@users.noreply.github.com>
2024-09-21 16:49:34 +0530

    Add files via upload

---
+++ StudentGrade.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class StudentGrade
+{
+	public static void main(String[] args)
+	{
+		Scanner v=new Scanner(System.in);
+		System.out.println("Enter the Total Subjects:");
+		int n=v.nextInt();
+		int arr[];
+		arr=new int[n];
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Enter The Marks of Subject["+(i+1)+"]:");
+			arr[i]=v.nextInt();
+			sum+=arr[i];
+		}
+		System.out.println("Sum Is:",sum);
+		int avg=(sum/n);
+		System.out.println("Average is:",avg);
+		int perc=avg/100
+		if(perc>=90)
+		{
+			System.out.println("Obtained Grade is: A");
+		}
+		else if(perc>=80)
+		{
+			System.out.println("Obtained Grade is: B");
+		}
+		else if(perc>=60)
+		{
+			System.out.println("Obtained Grade is: C");
+		}
+		else if(perc>=40)
+		{
+			System.out.println("Obtained Grade is: D");
+		}
+		else
+		{
+			System.out.println("Obtained Grade is: F");
+		}
+	}
+}
